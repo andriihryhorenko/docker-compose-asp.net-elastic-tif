f68f6db08b0c76bfe297577c4769948a7726925d
AndriiHryhorenko <hryhorenkoandriimail@gmail.com>
2022-08-31 19:25:35 +0200

    Initial commit
    docker-compose + configs + sample Asp.Net Core project

---
+++ README.md
@@ -0,0 +1,47 @@
+# Example Docker Compose project for Telegraf, InfluxDB and Grafana, Sample Asp.Net Core app, MondoDB, Elasticsearch + Kibana
+
+Sample docker-compose file with TIF, Elastisearch + Kibana, Asp.Net Core file, MongoDB
+
+
+## Start the stack with docker compose
+
+```bash
+$ docker-compose up
+```
+
+## Services and Ports
+
+### Grafana
+- URL: http://localhost:3000 
+- User: admin 
+- Password: admin 
+
+### Telegraf
+- Port: 8125 UDP (StatsD input)
+
+### InfluxDB
+- Port: 8086 (HTTP API)
+- User: admin 
+- Password: admin 
+- Database: influx
+
+### Elasticsearch
+- Port: 9200/9300
+
+### Kibana
+- URL: http://localhost:5601
+
+### Sample Asp.Net Core App
+- URL: http://localhost:8080
+
+### MongoDB
+- Port: 27017
+- User: admin 
+- Password: admin 
+
+
+
+## License
+
+The MIT License (MIT). Please see [License File](LICENSE) for more information.
+
---
+++ Sample2/Controllers/PrimeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace Sample2.Controllers
+{
+    public class PrimeController : Controller
+    {
+        [HttpGet]
+        public int GetNext(int prevNumber)
+        {
+            return Calculation(prevNumber);
+        }
+
+        private int Calculation(int number)
+        {
+            while (true)
+            {
+                bool isPrime = true;
+                //increment the number by 1 each time
+                number = number + 1;
+
+                int squaredNumber = (int)Math.Sqrt(number);
+
+                //start at 2 and increment by 1 until it gets to the squared number
+                for (int i = 2; i <= squaredNumber; i++)
+                {
+                    //how do I check all i's?
+                    if (number % i == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                if (isPrime)
+                    return number;
+            }
+        }
+    }
+}
